8882420298be4e086de48419201e6ac91c8a0a57
donghoon <donghoon_jeon@korea.ac.kr>
2020-06-04 17:11:58 +0900

    dh

--- analyzer/init.py
+++ analyzer/init.py
@@ -7,7 +7,8 @@ class Init():
     f = open(in_f,"r")
     example_data  = f.readlines()
     func_data = []
-    text_data = []
+    #text_data = []
+    text_num = 0    
 
     for i in range(0,len(example_data)):
         if('__unwind' in example_data[i]):
@@ -31,11 +32,19 @@ class Init():
         f.write(func_data[i])
     f.close()
     print("\n\n\n\n\n")
-
+ 
     for i in range(0,len(example_data)):
         if('.text' in example_data[i] and(len(example_data[i])>23)):
-#        print(example_data[i],end='   ')
-            text_data = text_data + [example_data[i]]
+            #text_data = text_data + [example_data[i]]
+            text_num = text_num + 1
+
+    text_data = [0]*text_num
+    j = 0
+    for i in range(0,len(example_data)):
+        if('.text' in example_data[i] and(len(example_data[i])>23)):
+           text_data[j] = example_data[i] 
+           j = j +1
+
 
 
     f = open("funcText","w")
