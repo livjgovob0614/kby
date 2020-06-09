aa5f18c695916c917720401547630bf8e16216bf
livjgovob0614 <livjgovob@gmail.com>
2020-06-09 17:49:21 +0900

    0609

--- impl_sample/test.py
+++
@@ -1,5 +0,0 @@
-a = 5
-if a & 1:
-  print ("1")
-else:
-  print ("0")
--- test.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-t = datetime.now()
-#output_dir = "{:%y}{:%m}{:%d}_{:%H}{:%M}{:%S}".format(t.year, t.month, t.day, t.hour, t.minute, t.second)
-#output_dir = "{%y}{%m}{%d}_{%H}{%M}{%S}".format(t.year, t.month, t.day, t.hour, t.minute, t.second)
-#output_dir = "{:%y:%m:%d:%H:%M:%S}".format(t.year, t.month, t.day, t.hour, t.minute, t.second)
-output_dir = t.strftime("%y")+t.strftime("%m")+t.strftime("%d")+"_"+t.strftime("%H")+t.strftime("%M")+t.strftime("%S")
-print (output_dir)
